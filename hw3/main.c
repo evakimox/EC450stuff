b2c267ed812d392dbaad30938d1ddb61c67757d2
Jingning <zhangjn@bu.edu>
2016-03-16 02:34:23 -0400

    Buttons done lack testing properly
    All button interruptions are tested on P1.3 only.
    Note seperation added

--- hw3/main.c
+++ hw3/main.c
@@ -10,9 +10,12 @@
 #define SCORE 0x80				//P1.7 button
 #define ALLBUTTONS 0xfc			//All the P1.x except P1.0, 1.1
 
+volatile int PlayingNote = 0;
 unsigned int playflag=0;
+volatile unsigned int restartflag = 0;
 volatile float TimeCount=0;
 volatile unsigned int speedDivisor=3;
+volatile unsigned int scorechanger = 1;
 
 const unsigned int toneArray[4]={497, 526, 591, 663};
 const float notelength[4]={2, 1.5, 0.5, 4};
@@ -23,63 +26,74 @@ void initializebuttons();
 void PlaySong();
 
 void main(void){
-	//Set watchdog and enable it's interrupt
+	//Use WDT to control how long each tone is
 	WDTCTL = (WDTPW + WDTTMSEL + WDTCNTCL + 1);
     IE1 |= WDTIE;
 
-    //Set timing
 	BCSCTL1 = CALBC1_1MHZ;
 	DCOCTL = CALDCO_1MHZ;
 
-	//Initialize timers, buttons
+	//Call timers, buttons interrupts
 	initializebuttons();
 	initTimerA();
 	TimeCount=0;
-	//Enable global interrupts
+
 	_bis_SR_register(GIE);
 
-	//Processing Loop
+	//call to play song
 	while(1){
-		if (playflag){PlaySong(); playflag=0;}	//Play song then reset flag.
-		else;
+		if (playflag){
+			PlaySong();
+			playflag=0;
+		}	//play only once
 	}
 }
 
 void initTimerA(){
-	TA0CTL |= TACLR; 	//reset clock
-	TA0CTL = TASSEL1+ID_0+MC_1;					//clock source is SMCLK, clk divider =1, up mode
-	TA0CCTL0 = CCIE;							//turn on output compare interrupt, initially OUTMOD
-	TA0CCR0 = 500;					//when output is triggered. TO make C_tone, eq => 261Hz= 1MHz/(x) ==> x=1MHz/(2*C_tone)
+	//set timer basics
+	TA0CTL |= TACLR;
+	TA0CTL = TASSEL1+ID_0+MC_1;
+	TA0CCTL0 = CCIE;
+	TA0CCR0 = 500;
 
-	//Connect Timer output to pin TA0
+	//Connect Timer output to pin P1.1
 	P1SEL |= TA0_BIT;
 	P1DIR |= TA0_BIT;
 }
 
-/*------------------------------------------*/
+//playsong function
 void PlaySong(){
 	//Internal Variables
 	unsigned int playingSong=1;
-	unsigned int PlayingNote=0;
 
 	//Play until the counter hits the end
 	while (playingSong){
-		TACCR0 = toneArray[PlayingNote]-1;							//selects which tone
+		TACCR0 = (toneArray[PlayingNote]/scorechanger)-1;
 		TimeCount=0;													//set TimeCount to 0
-		TACCTL0 = CCIE + OUTMOD_4;										//turn sound on for tone
+		TACCTL0 = CCIE + OUTMOD_4;
 		while (TimeCount < notelength[PlayingNote]/speedDivisor){
-			//do nothing
-			//And wait for the sound to play it's full length
+			//catch the restart flag at any time
+			if(restartflag){
+				PlayingNote = -1;
+				restartflag = 0;
+			}
 		}
-		TimeCount=0;													//reset TimeCount
-		TACCTL0 = CCIE;													//turn sound off for rest
-		if (PlayingNote >= 3){playingSong=0;}						//if we are done playing, bring flag down which will exit loop/function
-		else{PlayingNote+=1;}
-	}//end while
-
+		TimeCount=0;				//set TimeCount to 0
+		TACCTL0 = CCIE;				//turn sound off when one tone finishes
+		while(TimeCount < 0.08){
+			//do nothing and wait, this is notes seperator
+		}
+		TimeCount = 0;
+		if (PlayingNote >= 3){	//when the whole piece is finished
+			playingSong = 0;
+			PlayingNote = 0;
+		}
+		else{
+			PlayingNote += 1;
+		}
+	}//end outer while loop
 }//end PlaySong
 
-/*------------------------------------------*/
 
 void initializebuttons(){
     //Initialize buttons
@@ -95,18 +109,20 @@ void interrupt button_handler(){
 	//pause
 	if (P1IFG & PAUSE){
 		P1IFG &= ~PAUSE;	//reset the interrupt flag
-		playflag ^= 1;		//play and stop
-		TACCTL0 ^= OUTMOD_4;	//turn off sound
+		playflag = 0;		//play and stop
+		TACCTL0 = CCIE;	//turn off sound
 	}
 	//restart
 	else if(P1IFG & START){
 		P1IFG &= ~START;	//reset the interrupt flag
-		//do things according to start
+		playflag = 1;
+		restartflag = 1;
 	}
 	//resume from pause
 	else if(P1IFG & RESUME){
 		P1IFG &= ~RESUME;	//reset the interrupt flag
-		//do things according to resume
+		playflag = 1;
+		TACCTL0 = OUTMOD_4;		//turn on the sound and actually do nothing else
 	}
 	//speed up
 	else if(P1IFG & SPEEDUP){
@@ -126,7 +142,12 @@ void interrupt button_handler(){
 	//change the score
 	else if(P1IFG & SCORE){
 		P1IFG &= ~SCORE;	//reset the interrupt flag
-		//changes to the score
+		if(scorechanger == 1){
+			scorechanger = 2;
+		}
+		else{
+			scorechanger = 1;
+		}
 	}
 	else{
 		//do nothing
@@ -138,13 +159,12 @@ void interrupt AnotherWDT(){
 }
 
 void interrupt WDT_interval_handler(){
-	//If playing the song, add the time between to the TimeCount
+	//count the time that a note is played
 	if (playflag){
 		TimeCount += 0.01;
 	}
 	else{
-		TimeCount=0;
-
+		TimeCount=0;	//reset counter
 	}
 }
 
